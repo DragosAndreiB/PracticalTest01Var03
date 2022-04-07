83d78236a297f60d7bf1d66793f66e6074a947a2
dragosandreib <100837017+DragosAndreiB@users.noreply.github.com>
2022-04-07 13:11:40 +0300

    task B

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/Constants.java
@@ -0,0 +1,8 @@
+package ro.pub.cs.systems.eim.practicaltest01var03;
+
+public interface Constants {
+    String NUMBER1 = "1";
+    String NUMBER2 = "1";
+    String OPERATION = "0";
+    int SECONDARY_ACTIVITY_REQUEST_CODE = 122;
+}
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
@@ -2,13 +2,101 @@ package ro.pub.cs.systems.eim.practicaltest01var03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+
+    private EditText text1;
+    private EditText text2;
+    private TextView text3;
+    private Button buttonplus;
+    private Button buttonminus;
+    private Button buttonnext;
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            try {
+                int Value = Integer.parseInt(text1.getText().toString());
+                Value = Integer.parseInt(text2.getText().toString());
+                switch(view.getId()) {
+                    case R.id.button2:
+                        int sum = Integer.parseInt(text1.getText().toString());
+                        sum += Integer.parseInt(text2.getText().toString());
+                        text3.setText(text1.getText().toString() + " + " +
+                                text2.getText().toString() + " = " + sum);
+
+                        break;
+                    case R.id.button:
+                        int dif = Integer.parseInt(text1.getText().toString());
+                        dif -= Integer.parseInt(text2.getText().toString());
+                        text3.setText(text1.getText().toString() + " - " +
+                                text2.getText().toString() + " = " + dif);
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                Toast.makeText(getApplicationContext(), "One of inputs is not number",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(Constants.NUMBER1, text1.getText().toString());
+        savedInstanceState.putString(Constants.NUMBER2, text2.getText().toString());
+        savedInstanceState.putString(Constants.OPERATION, text3.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey(Constants.NUMBER1)) {
+            text1.setText(savedInstanceState.getString(Constants.NUMBER1));
+        }
+        if (savedInstanceState.containsKey(Constants.NUMBER2)) {
+            text2.setText(savedInstanceState.getString(Constants.NUMBER2));
+        }
+        if (savedInstanceState.containsKey(Constants.NUMBER2)) {
+            text3.setText(savedInstanceState.getString(Constants.OPERATION));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var03_main);
+
+        text1 = (EditText)findViewById(R.id.textView);
+        text2 = (EditText)findViewById(R.id.textView4);
+        text3 = (TextView)findViewById(R.id.textView6);
+        buttonplus = (Button)findViewById(R.id.button2);
+        buttonplus.setOnClickListener(buttonClickListener);
+        buttonminus = (Button)findViewById(R.id.button);
+        buttonminus.setOnClickListener(buttonClickListener);
+        buttonnext = (Button)findViewById(R.id.button3);
+        buttonnext.setOnClickListener(buttonClickListener);
+
+        if(savedInstanceState != null){
+            if(savedInstanceState.containsKey(Constants.NUMBER1)){
+                text1.setText(savedInstanceState.getString(Constants.NUMBER1));
+            }
+            if(savedInstanceState.containsKey(Constants.NUMBER2)){
+                text2.setText(savedInstanceState.getString(Constants.NUMBER2));
+            }
+            if(savedInstanceState.containsKey(Constants.OPERATION)){
+                text3.setText(savedInstanceState.getString(Constants.OPERATION));
+            }
+            Toast.makeText(getApplicationContext(), text3.getText().toString(),
+                                                                Toast.LENGTH_LONG).show();
+        }
     }
 }
